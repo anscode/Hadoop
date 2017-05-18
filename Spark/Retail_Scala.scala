22f074899729bedcdebe4b43d639423f37686906
anscode <a.anish@outlook.com>
2017-05-18 16:20:15 +1000

    test

--- Spark/Retail_Scala.scala
+++ Spark/Retail_Scala.scala
@@ -18,13 +18,20 @@ val orderDTAmountRDD =orderJoin.map(m=> (m._2._1.split(",")(1), m._2._2.split(",
 val orderByDateRDD = orderDTAmountRDD.reduceByKey((x,y) => x+y)
 orderByDateRDD.collect().foreach(println)
 
-//using RDD registerTempTable
+//using Dataset 
+//Datasets: Type-safe operations.
+//DataFrame: Untyped operations.
+//Dataset vs. DataFrame
+//A Dataset[T] is a parameterized type, where the type T is specified by the user and is associated with each element of the Dataset. 
+//A DataFrame, on the other hand, has no explicit type associated with it at compile time, from the user's point of view. Internally, 
+//a DataFrame is defined as a Dataset[Row], where Row is a generic row type defined by Spark SQL.
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 case class Order(OrderID : Int, OrderDate : String , CustomerID : Int, Status : String)
-val order = orderFile.filter(f=> f != orderFirstRow).map(m=> m.split(",")).map(m=> Order(m(0).toInt, m(1), m(2).toInt,m(3))).toDF()
+val order = orderFile.filter(f=> f != orderFirstRow).map(m=> m.split(",")).map(m=> Order(m(0).toInt, m(1), m(2).toInt,m(3))).toDS()
 order.registerTempTable("Order")
 
 case class OrderDetail(OrderItemID: Int, OrderID : Int,ProductID:Int, Quantity:Int,SubTotal:Float, Price: Float)
-val orderDetail = orderDetailFile.filter(f => f != orderDetailFirstRow).map(m=>m.split(",")).map(m=>OrderDetail(m(0).toInt,m(1).toInt,m(2).toInt,m(3).toInt,m(4).toFloat,m(5).toFloat)).toDF()
+val orderDetail = orderDetailFile.filter(f => f != orderDetailFirstRow).map(m=>m.split(",")).map(m=>OrderDetail(m(0).toInt,m(1).toInt,m(2).toInt,m(3).toInt,m(4).toFloat,m(5).toFloat)).toDS()
 orderDetail.registerTempTable("OrderDetail")
 
 val orderByDate=sqlContext.sql("select o.OrderDate, sum(od.SubTotal) as Total from Order o inner join OrderDetail od on o.OrderID=od.OrderID group by o.OrderDate")
